68a79a52f449ada0de9479719a774140c630a96b
Todd Blackman <tdblackman@gmail.com>
2015-03-07 21:17:03 -0500

    chef

--- chef/cookbooks/solo.rb
+++ chef/cookbooks/solo.rb
@@ -3,13 +3,16 @@ require 'yaml'
 
 path = File.realpath(File.dirname(__FILE__))
 
-a = YAML.load_file(File.join(path, 'runlist.yml'))
-b = YAML.load_file(File.join(path, 'chef_secrets.yml'))
+#a = YAML.load_file(File.join(path, 'runlist.yml'))
+chef_secrets = YAML.load_file(File.join(path, 'chef_secrets.yml'))
 
-a.merge!(b)
+chef_secrets['run_list'] = [
+  'role[test]'
+]
 
-File.write("#{path}/node.json", a.to_json)
+File.write("#{path}/node.json", chef_secrets.to_json)
 
-file_cache_path "#{path}/.cache"
-cookbook_path   path
-json_attribs    "#{path}/node.json"
+file_cache_path  "#{path}/.cache"
+cookbook_path    "#{path}"
+json_attribs     "#{path}/node.json"
+role_path        "#{path}/roles"
